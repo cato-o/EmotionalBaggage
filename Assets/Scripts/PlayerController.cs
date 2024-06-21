f3de9659e8af228d8df9683f5446119b13b8ee98
oohalakkadi <ooha.reddy@duke.edu>
2024-06-21 16:59:24 -0700

    Revert "bug fix"
    This reverts commit 96b57f22044fdaab7f7a90d5f593f59a2d7f5b1a.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -388,11 +388,6 @@ namespace EmotionalBaggage.Player
             {
                 return true;
             }
-            // bypass ramp grounding inactivation
-            if (Time.time - lastRampTime <= rampProtectionTime)
-            {
-                return true;
-            }
             return false;
         }
 
